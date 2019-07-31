9bbf9a9e5256572ba1926dee875e606a4e345d6e
Jonathan143 <1439821144@qq.com>
2019-07-31 20:39:21 +0800

    加载苹方字体

--- src/App.vue
+++ src/App.vue
@@ -12,4 +12,18 @@ export default class App extends Vue {}
 </script>
 
 <style>
+#app {
+  color: #545454;
+  font-family: 'pingFangSC-Medium';
+}
+@font-face {
+  font-family: 'pingFangSC-Medium';
+  src: url('./theme/PingFang Medium.ttf');
+  font-weight: normal;
+  font-style: normal;
+}
+::-webkit-scrollbar {
+  width: 0;
+  height: 0;
+}
 </style>
